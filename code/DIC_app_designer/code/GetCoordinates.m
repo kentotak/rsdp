02095c673cb52bebf2815ecb299857083795df10
kentotak <kento.takahashi@kuleuven.be>
2026-04-17 13:14:01 +0200

    Simplification DIC application
    Multiple files were removed to fit the workflow

--- code/DIC_app_designer/code/GetCoordinates.m
+++
@@ -1,6 +0,0 @@
-% Get coordinates from displacement
-function Valid=GetCoordinates(Displ,FirstImage)  
-    SizeDispl=size(Displ);    
-    FirstImage=FirstImage*ones(1,SizeDispl(1,2));
-    Valid=Displ+FirstImage;
-
--- code/DIC_app_designer/code/GetDisplacementGrid.m
+++
@@ -1,7 +0,0 @@
-% Get displacement from coordinates w.r.t. grid
-function Displ=GetDisplacementGrid(Valid,Grid)
-    SizeValid=size(Valid);    
-    ValidGrid=Grid(:,1)*ones(1,SizeValid(1,2));
-    Displ=Valid-ValidGrid;
-end
-
